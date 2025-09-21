7e7e35cdd3efa51d0abfb126e4625872e4c5c17d
Naman Tiwari <www.skynetc52@gmail.com>
2025-09-21 14:37:49 +0530

    allow from all origins

--- server/src/server.js
+++ server/src/server.js
@@ -16,9 +16,10 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || "http://localhost:8080",
+  origin: "*",
   credentials: true
 }));
+app.options("*", cors());
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
