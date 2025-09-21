9eeefa6d8168c62c56146fc414a17c6c6a8ad3c2
Naman Tiwari <www.skynetc52@gmail.com>
2025-09-21 13:22:40 +0530

    updating the dev script

--- client/src/lib/apiCitizen.js
+++ client/src/lib/apiCitizen.js
@@ -1,16 +1,17 @@
 import axios from "axios";
+const apiUrl = import.meta.env.SERVER_URL;
 
 export const citizenOTPApi = async(phone) =>{
-    const response = await axios.post("http://localhost:5000/api/auth/citizen/login", {phone});
+    const response = await axios.post(`${apiUrl}/api/auth/citizen/login`, {phone});
     return response.data;
 }
 
 export const citizenVerifyOTP = async(phone, otp) => {
-    const response = await axios.post("http://localhost:5000/api/auth/citizen/verify-otp", {phone, otp});
+    const response = await axios.post(`${apiUrl}/api/auth/citizen/verify-otp`, {phone, otp});
     return response.data;
 }
 
 export const citizenSubmitName = async(phone, name) => {
-    const response = await axios.patch("http://localhost:5000/api/auth/citizen/submit-name", {phone, name});
+    const response = await axios.patch(`${apiUrl}/api/auth/citizen/submit-name`, {phone, name});
     return response.data;
 }
